5e5836a07ea4f2c5c4e55860808ba3e70ce87fbc
Johan Runesson <info@johanrunesson.se>
2024-04-09 15:47:33 +0200

    feat: add new plugins and settings

--- plugins.lua
+++ plugins.lua
@@ -51,13 +51,62 @@ local plugins = {
     "folke/todo-comments.nvim",
     lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = { },
+    config = function() end,
+    opts = {},
   },
-  
+
   {
     "wuelnerdotexe/vim-astro",
     lazy = false,
-    opts = { },
+    config = function() end,
+    opts = {},
+  },
+
+  {
+    "mustache/vim-mustache-handlebars",
+    lazy = false,
+    config = function() end,
+    opts = {},
+  },
+  {
+    "folke/trouble.nvim",
+    lazy = false,
+    branch = "dev", -- IMPORTANT!
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+    opts = {
+      multiline = true,
+    }, -- for default options, refer to the configuration section for custom setup.
   },
 
   -- To make a plugin not be loaded
